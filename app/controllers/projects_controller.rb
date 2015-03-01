ba3152ef9016e73a823ca39468d0d7eeb2ef5c87
Andre Rocha <andre.marques747@hotmail.com>
2015-03-01 20:22:19 -0300

    Criação de Metodos create, show, destroy, edit na controller projects - criação de validação de obrigatoriedade na model project - Termino de construção partial view form e views index, show e edit

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -17,19 +17,30 @@ class ProjectsController < ApplicationController
   end
   
   def show
+    @project = Project.find_by_id(params[:id])
   end
 
   def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to @project
   end
 
   def update
+    @project = Project.find(params[:id])
+  if @project.update(project_params)
+  redirect_to @project
+  else
+    render "edit"
   end
-
+end
+  
   def edit
+     @project = Project.find(params[:id])
   end
 
   private
   def project_params
-    params.require(:project).permit(:name, :descripition)
+    params.require(:project).permit(:name, :description)
   end
 end
