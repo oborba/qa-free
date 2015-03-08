73c9d051f4c9ab2ba6386e2cb0a470bb6a6f56dd
Diego Alves <diego.oborba@gmail.com>
2015-03-08 01:45:33 -0300

    fix Index and New test

--- spec/controllers/test_plans_controller_spec.rb
+++ spec/controllers/test_plans_controller_spec.rb
@@ -1,20 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe TestPlansController, type: :controller do
-
-let(:plan_params) {{id: "#{Time.now.to_i}", test_plan_name: "Plano de teste", test_plan_description: nil, created_at: "2015-03-02 20:45:20", updated_at: "2015-03-02 20:45:20"}}
-let(:plan_params1) {{id: 1, test_plan_name: "Plano de teste", test_plan_description: nil, created_at: "2015-03-02 20:45:20", updated_at: "2015-03-02 20:45:20"}}
-let(:test_plan_created) {TestPlan.create(plan_params)}
+  let(:project) {Project.create!({:name => "Project Name", :description => "Project Description"}) }
+  let(:plan_params) {{id: "#{Time.now.to_i}", test_plan_name: "Plano de teste", test_plan_description: nil, created_at: "2015-03-02 20:45:20", updated_at: "2015-03-02 20:45:20"}}
+  let(:plan_params1) {{id: 1, test_plan_name: "Plano de teste", test_plan_description: nil, created_at: "2015-03-02 20:45:20", updated_at: "2015-03-02 20:45:20"}}
+  let(:test_plan_created) {TestPlan.create(plan_params)}
+  
   describe "GET #index" do
     it "returns http success" do
-      get :index
+      get :index, project_id: project
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET #new" do
     it "returns http success" do
-      get :new
+      get :new, project_id: project
       expect(response).to have_http_status(:success)
     end
   end
@@ -22,8 +23,7 @@ let(:test_plan_created) {TestPlan.create(plan_params)}
   describe "GET #create" do
     it "returns http success" do
       post :create, :test_plan => (plan_params)
-      test_plan_new = test_plan_created
-      expect(TestPlan.last).to eq(test_plan_new)
+      expect(TestPlan.last).to eq(test_plan_created)
     end
   end
 
