25265b1fe966ecd5d862b9913fe9129e1cc4bf46
Andre Rocha <andre.marques747@hotmail.com>
2015-03-10 12:04:39 -0300

    Appending options Low Medium Critical in the critical field, creating critical validation model test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,5 @@
 class Test < ActiveRecord::Base
   	validates_presence_of :title, :description, :criticality, :time
+  	validates_length_of :title, maximum: 100
+  	validates :criticality, inclusion: { in: %w(Low Medium Critical)}
 end
