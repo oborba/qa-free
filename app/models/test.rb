43c299652de4b0d434bf15053bd6b77d493b972b
Diego Alves <diego@diego.oborba@gmail.com>
2015-03-12 14:42:38 -0300

    description is not required

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,7 @@
 class Test < ActiveRecord::Base
   has_and_belongs_to_many :test_plans
 
-  validates_presence_of :title, :description, :criticality, :time
+  validates_presence_of :title, :criticality, :time
   validates_length_of :title, maximum: 100
   validates :criticality, inclusion: { in: %w(Low Medium Critical)}
 end
