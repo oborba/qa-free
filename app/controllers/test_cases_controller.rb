e7dc21da7eefee8244d9ad32ab2c4d0fcfa0e230
Diego Alves <diego.oborba@gmail.com>
2015-03-16 20:40:33 -0300

    change name

---
+++ app/controllers/test_cases_controller.rb
@@ -0,0 +1,59 @@
+class TestCasesController < ApplicationController
+  before_filter :check_user_logged_in!
+
+
+  def index
+    @test = TestCase.all
+  end
+
+  def new
+    @test = TestCase.new
+  end
+
+  def create
+    @test = TestCase.new(test_params)
+    if @test.save
+      redirect_to @test
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @test = TestCase.find_by_id(params[:id])
+  end
+
+  def destroy
+    @test = TestCase.find(params[:id])
+    @test.destroy
+    redirect_to @test
+  end
+
+  def update
+    @test = TestCase.find(params[:id])
+    if @test.update(test_params)
+      redirect_to @test
+    else
+      render "edit"
+    end
+  end
+  
+  def edit
+    @test = TestCase.find(params[:id])
+  end
+  
+  private
+  def test_params
+    params.require(:test).permit(:title, :description, :criticality, :time)
+  end
+
+  def check_admin_logged_in!
+    authenticate_admin!
+  end
+  
+  def check_user_logged_in!
+    if !admin_signed_in?
+      authenticate_user!
+    end   
+  end
+end
