f1b31362a60fff4d36cc85970ba6da1bc79a0238
Diego Alves <diego@diego.oborba@gmail.com>
2015-03-17 11:47:20 -0300

    fix migrate

--- db/migrate/20150316232436_create_correct.rb
+++ db/migrate/20150316232436_create_correct.rb
@@ -1,7 +1,5 @@
 class CreateCorrect < ActiveRecord::Migration
   def change
-  	drop_table :test_cases_plans
-  	
     create_join_table :test_plans, :test_cases do |t|
     	t.index [:test_plan_id, :test_case_id]
     	t.index [:test_case_id, :test_plan_id]
