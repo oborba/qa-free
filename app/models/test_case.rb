f004beaae9034eff5b6afb0a5bbfeec84bddffef
Diego Alves <diego.oborba@gmail.com>
2015-03-16 21:08:49 -0300

    change test to test_case

---
+++ app/models/test_case.rb
@@ -0,0 +1,7 @@
+class TestCase < ActiveRecord::Base
+  has_and_belongs_to_many :test_plans
+
+  validates_presence_of :title, :criticality, :time
+  validates_length_of :title, maximum: 100
+  validates :criticality, inclusion: { in: %w(Low Medium Critical)}
+end
