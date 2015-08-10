912e18693aab0d9ce8fe0cd643cc482174ccdd33
Diego Alves <diego.oborba@gmail.com>
2015-08-09 23:23:16 -0300

    Setup application defaults

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
- project = Project.create({ name: "Default Project", description: "Default Description"})
- test_plan =TestPlan.create({test_plan_name: "Default Test Plan", test_plan_description: "Default Description", project: project})
- admin = Admin.create({email: "admin@default.com", password: "inicial1234", password_confirmation: "inicial1234"})
+puts "\nCreate default admin to use the system\n"
+
+admin = Admin.create(
+  {
+    email: "admin@default.com",
+    password: "inicial1234",
+    password_confirmation: "inicial1234"
+  }
+)
+
+puts "Login #{admin.email}"
+puts "Pass  #{admin.password}"
+
+puts "\nCreate project and test plan examples\n"
+
+project = Project.create(
+  { 
+    name: "Default Project",
+    description: "Default Description"
+  }
+)
+
+TestPlan.create(
+  {
+    test_plan_name: "Default Test Plan",
+    test_plan_description: "Default Description", 
+    project: project
+  }
+)
+
+puts "\nUrl localhost:3000/admins/sign_in \n"
