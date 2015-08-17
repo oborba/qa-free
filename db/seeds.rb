2c91bab95a4b78bfc65faf13aa10e0a8fd7c2450
Diego Alves <diego.oborba@gmail.com>
2015-08-17 00:03:33 -0300

    Create default user in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,21 @@ admin = Admin.create(
   }
 )
 
-puts "Login #{admin.email}"
-puts "Pass  #{admin.password}"
+puts "Admin Login #{admin.email}"
+puts "Admin Pass  #{admin.password}"
+
+puts "\nCreate default user"
+
+user = User.create(
+  {
+    email: "user@default.com",
+    password: "inicial1234",
+    password_confirmation: "inicial1234"
+  }
+)
+
+puts "User Login #{user.email}" 
+puts "User Pass #{user.password}"
 
 puts "\nCreate project and test plan examples\n"
 
@@ -28,4 +41,5 @@ TestPlan.create(
   }
 )
 
-puts "\nUrl localhost:3000/admins/sign_in \n"
+puts "\nAdmin Url localhost:3000/admins/sign_in \n"
+puts "\nUser Url localhost:3000/users/sign_in \n"
